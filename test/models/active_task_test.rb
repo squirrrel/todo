7a3bd488bd5366987a51e360ab6f87968dcec3b6
Nina Moskalyk <nina.moskalyk@gmail.com>
2013-09-30 14:38:38 +0300

    removing extra files

--- test/models/active_task_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ActiveTaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/basic_task_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BasicTaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
