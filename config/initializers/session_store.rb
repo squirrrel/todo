cfba7fc86ca57d7782c502e223c2a67a91002beb
Nina Moskalyk <nina.moskalyk@gmail.com>
2013-11-06 15:35:58 +0200

    yikes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,5 +6,5 @@ mem_config = mem_config[Rails.env]
 mem_servers = mem_config['host'].split(' ').map{|h| "#{h}:#{mem_config['port']}"}
 ENV['MEMCACHE_SERVERS'] = mem_servers.join(' ')
 
-Rails.application.config.session_store :dalli_store, memcache_server: ENV['MEMCACHE_SERVERS'], {username: '5664af', password: '28eebde096',
-													 namespace: 'sessions', key: '_todo_session', expire_after: 30.minutes}
+Rails.application.config.session_store :dalli_store, memcache_server: ENV['MEMCACHE_SERVERS'], username: '5664af', password: '28eebde096',
+													 namespace: 'sessions', key: '_todo_session', expire_after: 30.minutes
